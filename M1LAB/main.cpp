8fefde04fcab49ea89ed56df1e536b85037a0e87
isjmhh <143422892+isjmhh@users.noreply.github.com>
2023-08-28 11:25:55 -0400

    Started M1LAB

---
+++ M1LAB/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+CSC 134
+M1LAB - Apple Orchard
+Joel
+8/28
+Simple interactive store.
+*/
+
+int main()
+{
+    string name = "Joel";
+    cout << "Welcome to the " << name << " orchard." << endl;
+    return 0;
+}
