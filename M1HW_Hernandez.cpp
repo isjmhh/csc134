c35ec87b972d4ad107fe03ca127cbdc54818f6fd
isjmhh <hernandj2406@student.faytechcc.edu>
2023-09-11 11:35:02 -0400

    Finished

---
+++ M1HW_Hernandez.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+string name = "fight club";
+cout << "The movie " << name << " came out in 1999, and had a worldwide gross of 100.9 million" << endl;
+cout << "Favorite thing about the movie is bradd pitt" << endl;
+cout << "Best quote is \"Stop Trying to Control Everything and Just Let Go. Let Go!\"" << endl;
+cout << "Another good quote is \"I Felt Like Destroying Something Beautiful.\"" << endl;
+
+    return 0;
+}
