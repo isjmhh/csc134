5322e79d1a38f36c11ff59fed6eab8e149db8ef7
isjmhh <hernandj2406@student.faytechcc.edu>
2023-09-13 11:49:11 -0400

    M2HW Q4
    done

---
+++ M2HWQuestion4_Hernandez/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+string school = "FTCC";
+string cheer = "Let's go ";
+string mascot = "Trojans";
+cout << cheer << school << endl;
+cout << cheer << school << endl;
+cout << cheer << school << endl;
+cout << cheer << mascot << endl;
+
+    return 0;
+}
