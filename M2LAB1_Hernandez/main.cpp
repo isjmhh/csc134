0b2f2543e56a8f9d83489891a2ab68bb9aa372e4
isjmhh <hernandj2406@student.faytechcc.edu>
2023-09-13 10:53:58 -0400

    M2LAB
    Started lab

---
+++ M2LAB1_Hernandez/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/*
+M2LAB1 Crate Example
+Find th volume of a crate of a given size
+Find the cost tot make it
+Find th price we sell it for
+Find the profit
+*/
+
+using namespace std;
+
+int main()
+{
+    cout << "General Crates Pricing System" << endl;
+    //declare the variables
+    const double COST_PER_CUBIC_FOOT = 0.23; //percubic foot
+    const double CHARGE_PER_CUBIC_FOOT = 0.5; // $ per cubic foot
+
+    double length, width,
+    return 0;
+}
