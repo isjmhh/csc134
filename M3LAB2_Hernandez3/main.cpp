6d959595f95ad11549a67695efbade186bb4d380
isjmhh <hernandj2406@student.faytechcc.edu>
2023-09-24 16:19:04 -0400

    M3LAB
    done

--- M3LAB2_Hernandez3/main.cpp
+++ M3LAB2_Hernandez3/main.cpp
@@ -30,7 +30,8 @@ void chooseHouse1();
 void chooseHouse2();
 void chooseStairs1();
 void chooseStairs2();
-
+void chooseDoor1();
+void chooseDoor2();
 // the lines above tell the program that these functions will
 // exist, but we have to define them later on in the file.
 ////
@@ -56,13 +57,25 @@ int main() {
     cin >> choice;
     if (1 == choice) {
    chooseStairs1();
+   cout << "Choose door1 to your right or door2 to your left" << endl;
+   cout << "Type 1 or 2: " << endl;
+   cin >> choice;
+   if (1 == choice) {
+    chooseDoor1();
+   }
+    else if (2 == choice) {
+        chooseDoor2 ();
+
+
+
     }
-   else if (2 == choice) {
+   else if (2 == choice)
     chooseStairs2();
    }
   }
   else if (2 == choice) {
     chooseHouse2();
+    cout << "you die while trying to defeat the zombies!!" << endl;
   }
   else {
     cout << "I'm sorry, that is not a valid choice, weirdo lol." << endl;
@@ -99,6 +112,13 @@ void chooseStairs1() {
 void chooseStairs2() {
     cout << "You chose stairs 2" << endl;
     cout << "You see a door in front of you" << endl;
+}
+void chooseDoor1() {
+cout << "you chose door 1 to your right" << endl;
+cout << "You open the door and die to demons. YOU LOSE.." << endl;
+}
+void chooseDoor2() {
+cout << "you chose door 2 and WIN the game. YOU WIN.." <<endl;
 }
 
 // If we had a Door #3, or 4, we would add another else if to our
