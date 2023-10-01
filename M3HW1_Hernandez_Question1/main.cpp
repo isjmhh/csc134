d26d76ab0bacdae543a51fd9ff0f261a61034d91
isjmhh <hernandj2406@student.faytechcc.edu>
2023-10-01 16:35:00 -0400

    M3HW1_Q1_Hernandez

--- M3HW1_Hernandez_Question1/main.cpp
+++ M3HW1_Hernandez_Question1/main.cpp
@@ -11,6 +11,8 @@ Joel
 // declare
 void Answeryes();
 void Answerno();
+void Answer1();
+void Answer2();
 
 
 int main()
@@ -27,6 +29,15 @@ int main()
     // branch on the choice
     if (1 == choice) {
         Answeryes();
+        cout << "Do you like school?" << endl;
+        cout << " 1 for yes 2 for no " << endl;
+        cin >> choice;
+        if (1 == choice) {
+            Answer1();
+        }
+        else if (2 == choice) {
+            Answer2();
+        }
     }
     else if (2 == choice) {
         Answerno();
@@ -44,4 +55,11 @@ cout << "That is great to hear i hope to be of service!" << endl;
 
 void Answerno() {
 cout << "That's not good but i hope you will learn to like me?" << endl;
+}
+
+void Answer1() {
+cout << " Weirdo lol but why" << endl;
+}
+void Answer2() {
+cout << " Oh, so what do you like lol?" << endl;
 }
